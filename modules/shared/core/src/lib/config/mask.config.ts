423d7c9f1e24987afaab1b68cf38694c2f622408
phuong-tran-1999 <150643127+phuong-tran-1999@users.noreply.github.com>
2024-07-25 22:30:34 +0700

    Update UI

--- modules/shared/core/src/lib/config/mask.config.ts
+++ modules/shared/core/src/lib/config/mask.config.ts
@@ -3,6 +3,7 @@ import { IConfig } from 'ngx-mask';
 export const maskConfigFunction: () => Partial<IConfig> = () => {
     return {
         validation: false,
+        thousandSeparator: ',',
         separatorLimit: '999999999999999',
         decimalMarker: '.',
     };
